cd5d568aa74429ddd61378affc7f5d5aa6afa6fa
Student Name <tet@test.com>
2019-05-14 13:09:23 -0400

    chapter5

--- demos/src/app/app.module.ts
+++ demos/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from "./app.component";
+import { AlbumListComponent } from './albums/album-list/album-list.component';
+import { AlbumCardComponent } from './albums/album-card/album-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AlbumListComponent, AlbumCardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
