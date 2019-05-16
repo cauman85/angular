a133de02a4d4d4de69935e9711536656eb4c26e3
Student Name <tet@test.com>
2019-05-16 11:02:57 -0400

    chapter9

---
+++ demos/src/app/albums/album-details/album-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
+import { AlbumService } from '../shared/album.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-album-details',
+  templateUrl: './album-details.component.html',
+  styleUrls: ['./album-details.component.css']
+})
+
+export class AlbumDetailsComponent implements OnInit {
+
+    albums: Album;
+
+    constructor(
+      private route: ActivatedRoute,
+      private albumService: AlbumService
+    ) {}
+
+    ngOnInit() {
+      const id = +this.route.snapshot.paramMap.get("id");
+      this.albumService.getAlbumById(id).subscribe(
+        album => {
+          this.albums = album;
+        },
+        error => console.log("Error: ", error)
+      );
+    }
+
+}
